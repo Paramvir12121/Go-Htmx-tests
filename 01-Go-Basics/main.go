8b38a79874518400fb36ba214e4866fa430b30bc
paramvir12121 <paramvir12121@gmail.com>
2024-06-11 22:48:43 -0400

    second commit

--- 01-Go-Basics/main.go
+++ 01-Go-Basics/main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-func main () {
+func main() {
 
-	fmt.Println("Hello World")
+	name := "paramvir"
+
+	var num8 float64 = 250.7
+
+	fmt.Print("Hello World", name, num8)
+	fmt.Print("New Line?\n")
+	fmt.Printf("Name 1 %v num8 %v", name, num8)
 }
